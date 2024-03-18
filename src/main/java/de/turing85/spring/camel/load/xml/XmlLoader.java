d84cac1463db25975151d0e307895634c60e2ce7
Marco Bungart <marco.bungart@googlemail.com>
2024-03-18 18:41:48 +0100

    Simplify route loading

--- src/main/java/de/turing85/spring/camel/load/xml/XmlLoader.java
+++ src/main/java/de/turing85/spring/camel/load/xml/XmlLoader.java
@@ -1,16 +1,11 @@
 package de.turing85.spring.camel.load.xml;
 
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Optional;
-
 import lombok.RequiredArgsConstructor;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spi.Resource;
+import org.apache.camel.spi.ResourceLoader;
 import org.apache.camel.spi.RoutesLoader;
-import org.apache.camel.support.ResourceHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,17 +15,11 @@ public class XmlLoader extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
-    String fileName = "echoRoute.xml";
     // @formatter:off
-    URI echoRouteUrl = Optional.of(getClass())
-        .map(Class::getClassLoader)
-        .map(loader -> loader.getResource(fileName))
-        .orElseThrow()
-        .toURI();
-    // @formatter:on
-    Path echoRoutePath = Path.of(echoRouteUrl);
-    Resource routeResource = ResourceHelper.fromBytes(fileName, Files.readAllBytes(echoRoutePath));
-    // @formatter:off
+    Resource routeResource = context
+        .getCamelContextExtension()
+        .getContextPlugin(ResourceLoader.class)
+        .resolveResource("classpath:echoRoute.xml");
     context
         .getCamelContextExtension()
         .getContextPlugin(RoutesLoader.class)
